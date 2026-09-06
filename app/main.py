043ddc150a6dd70a93ce14f55efa789e519c6618
Sri Sakthi Priya <srisakthipriya22@gmail.com>
2026-09-06 21:56:56 +0530

    Add get users endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.schemas.user import UserCreate
-from app.services.user_service import create_user
+from app.services.user_service import create_user, get_all_users
 from app.config import APP_NAME
 
 app = FastAPI(title=APP_NAME)
@@ -9,6 +9,7 @@ app = FastAPI(title=APP_NAME)
 @app.get("/")
 def home():
     return {"message": "Welcome to the Task Management API"}
+
 
 @app.post("/users")
 def add_user(user: UserCreate):
@@ -22,3 +23,17 @@ def add_user(user: UserCreate):
         "name": new_user.name,
         "email": new_user.email
     }
+
+
+@app.get("/users")
+def get_users():
+    all_users = get_all_users()
+
+    return [
+        {
+            "id": user.id,
+            "name": user.name,
+            "email": user.email
+        }
+        for user in all_users
+    ]
